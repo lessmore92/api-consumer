a22b76db107391bbdcf97027a766735e29c7c42d
Lessmore92 <bahramisoft@gmail.com>
2020-02-11 01:41:41 +0330

    first sample test

---
+++ tests/ApiConsumerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: Lessmore92
+ * Date: 2/11/2020
+ * Time: 1:27 AM
+ */
+
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertTrue;
+
+class ApiConsumerTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        assertTrue(true);
+    }
+}
